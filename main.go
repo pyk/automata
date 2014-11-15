32893d5b11359450daf21ebec4aaea8092f7cd7f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-11-15 20:13:33 +0700

    add index handler & API custom mux
    custom mux for API error handling.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,15 +13,65 @@ var (
 	PORT = os.Getenv("AUTOMATA_SERVER_PORT")
 )
 
-// HelloWorld handle '/' request
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
+// apiError define structure of API error
+// TODO: API error response should return JSON
+type apiError struct {
+	Error   error  `json:"-"`
+	Message string `json:"error"`
+	Code    int    `json:"code"`
+}
+
+// apiHandler global API mux
+type apiHandler func(w http.ResponseWriter, r *http.Request) *apiError
+
+func (fn apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	// add header on every response
+	w.Header().Add("Server", "Automata/0.1")
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	// if handler return an &apiError
+	err := fn(w, r)
+	if err != nil {
+		// http log
+		log.Printf("%s %s %s %s", r.RemoteAddr, r.Method, r.URL, err.Error)
+
+		// response proper http status code
+		w.WriteHeader(err.Code)
+
+		// response JSON
+		resp := json.NewEncoder(w)
+		resp.Encode(err)
+
+		return
+	}
+
+	// http log
+	// TODO: print response status
+	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+}
+
+// index handle '/' request
+func index(w http.ResponseWriter, r *http.Request) *apiError {
+
+	// response "404 not found" on every undefined
+	// URL pattern handler
+	if r.URL.Path != "/" {
+		return &apiError{
+			errors.New("Not Found"),
+			"Not Found",
+			http.StatusNotFound,
+		}
+	}
+
 	fmt.Fprintln(w, "Hello World! - automata.")
+	return nil
 }
 
 func main() {
 
-	// rest api
-	http.HandleFunc("/", HelloWorld)
+	// register index handler
+	http.Handle("/", apiHandler(index))
 
 	// server listener
 	log.Printf("Listening on :%s", PORT)
